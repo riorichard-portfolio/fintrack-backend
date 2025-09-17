389c3e541f1b128eeee2f1b60873a9122acfce6f
riorichard98 <riorichard12@gmail.com>
2025-09-17 17:02:17 +0700

    feat: new postgres for postgres db

--- internal/infrastructure/db/postgres.go
+++ internal/infrastructure/db/postgres.go
@@ -1,1 +1,18 @@
 package db
+
+import (
+	"fintrack-backend/internal/infrastructure/config"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewPostgres(config config.Config) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(config.DBUrl()), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to postgres: %v", err)
+	}
+	return db
+
+}
